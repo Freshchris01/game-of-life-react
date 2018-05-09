11748c6ca71dc2a7969e4094051ee8166e501581
Christian D <christian.diemers@gmail.com>
2018-05-09 22:56:20 +0200

    Basic Game Logic (#8)
    * test commit
    
    * basic grid setup
    
    * Basic gameLogic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Game from './Game/Game.react';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React CI Working? Yeah!!</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+			<div className="App" style={styles}>
+				<Game />
 			</div>
 		);
 	}
+}
+
+const styles = {
+	display: 'flex',
+	height: '100vh'
 }
 
 export default App;
